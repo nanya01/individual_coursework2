177c4529fd3fcaeabcc96b461d29dcf7ba15a365
Peter <peterahunanya79@gmail.com>
2023-06-01 01:29:35 +0400

    added-mongo-collection

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 const express = require("express");
+var cors = require('cors')
 var path = require("path");
 var fs = require("fs");
 const app = express();
-
+ var mongodb = require('mongodb')
 let db; 
-var MongoClient = require('mongodb').MongoClient;  
+var MongoClient = mongodb.MongoClient;  
+const ObjectID = mongodb.ObjectID;
 var url = "mongodb+srv://nanya:nanya@cluster0.1oef9f0.mongodb.net/";  
+const MONGODB_USERNAME = "nanya";
+const MONGODB_PASSWORD = "nanya";
+const PROJECT_ID = "644a5e471f79d15a6cecb11f";
+const MONGODB_PUBLICKEY = "xiprooaw";
+const MONGODB_PRIVATEKEY = "533eb750-9290-4a18-8471-5d8e3f69b7ca";
+const MONGODB_CLUSTER = "Cluster0";
 MongoClient.connect(url, function(err, client) {  
 if (err) throw err;  
-db = client.db('products')
+db = client.db('product')
 console.log("Database created!");  
  
 });  
+app.use(express.json())
 
+
+app.use(cors())
 // logging middleware
 app.use(function(req, res, next){
     console.log("Request IP: " + req.url);
@@ -20,8 +31,9 @@ app.use(function(req, res, next){
    next();
 }); 
 
+
 // static file  middleware
-app. use(function(req, res, next){
+app.use(function(req, res, next){
     // uses path.join to find the path where the file should be
     var filePath = path.join(__dirname, "static", req.url);
     // Built-in fs.stat gets info about a file
@@ -31,23 +43,70 @@ app. use(function(req, res, next){
         return;
         }
 
-        if(fileInfo.isFile()) res.sendFile(filePath);
-        else next();
+        if(fileInfo.isFile()){
+            console.log("Found file");
+            res.sendFile(filePath);
+        }
+        else{
+            console.log("wrong file");
+            next();
+        }
     });
 })
 app.param('collectionName', (req, res, next, collectionName) => { 
      req.collection = db.collection(collectionName)
-      return next()})
-// post request 
+      return next()});
 
+      // GET all Request
+  app.get('/collection/:collectionName', (req, res, next) =>{
+   // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+     req.collection.find({}).toArray((e, result) =>{
+        if(e) return next(e)
+
+        res.send(result);
+     })
+  });
+  app.get('/collection/:collectionName/:search', (req, res, next) =>{
+      req.collection.findOne({
+        "topic": req.params.search
+      },(e, result) =>{
+         if(e) return next(e)
+ 
+         res.send(JSON.stringify(result));
+      })
+  
+   });
+
+   app.get('/collection/:collectionName/search', async (req, res) =>{
+        const searchTerm = req.query.q;
+        const searchResults = await collection.find({ $text: { $search: searchTerm } }).toArray();
+
+        res.json(searchResults);
+   });
+
+      //  POST request 
 app.post('/collection/:collectionName', (req, res, next) => {
-    req.collection.insert(req.body, (e, results) => {
+    //res.setHeader('Access-Control-Allow-Origin', '*');
+    req.collection.insert(req.body, (e, result) => {
             if(e) return next(e);
 
             res.send(result.ops)
 
     })
-})
+});
+
+app.put('/collection/:collectionName/:id', (req, res, next) => {
+    req.collection.update(
+        {_id: new ObjectID(req.params.id)},
+        {$set: req.body},
+        {safe:true, multi:false},
+        (e, result) =>{
+            if(e) return next(e)
+
+            res.send((result.result.n ===1) ? {msg: 'success'} : {msg: 'error'})
+        }
+    )
+});
 app.use(function(req, res){
         res.status(404);
 
